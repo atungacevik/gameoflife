a064d63af524c738d50d2d258cbc4c14f74fbc85
atungacevik <44293417+atungacevik@users.noreply.github.com>
2019-10-21 12:43:38 +0300

    Test Cases
    Implementation of board borders and controling neighbours

---
+++ game_of_life/test.py
@@ -0,0 +1,26 @@
+import g_o_l
+
+
+def board_testing():
+    row, col = 100, 100
+
+    n, m = 10, 300
+    assert g_o_l.existential_check(n, m, row, col) == False
+
+    n, m = -10, 100
+    assert g_o_l.existential_check(n, m, row, col) == False
+
+    n, m = 100, 100
+    assert g_o_l.existential_check(n, m, row, col) == True
+
+
+def neighbour_tester():
+    neighbour_hood = [[False, True], [True, False]]
+    result = [[2, 1], [1, 2]]
+    assert g_o_l.neighbour_finder(neighbour_hood) == result
+
+    neighbour_hood = [[False, True, False],
+                       [True, False, False],
+                       [True, False, True]]
+    result = [[2, 1, 1], [2, 4, 2], [1, 3, 0]]
+    assert g_o_l.neighbour_finder(neighbour_hood) == result
